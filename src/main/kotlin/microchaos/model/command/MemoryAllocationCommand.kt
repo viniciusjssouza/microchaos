c76aac2c607f8c774fa95a967a01340dff0901bc
Vinicius Souza <vinicius.souza@loft.com.br>
2020-11-02 17:32:45 -0300

    fix: jackson requires properties to be public

--- src/main/kotlin/microchaos/model/command/MemoryAllocationCommand.kt
+++ src/main/kotlin/microchaos/model/command/MemoryAllocationCommand.kt
@@ -1,17 +1,22 @@
 package microchaos.model.command
 
-import microchaos.infra.logging.loggerFor
 import java.util.*
 import kotlin.math.roundToInt
 
-class MemoryAllocationCommand(private val amount: Int) : Command() {
+// jackson requires that the constructor input be a public property :(
+// Issue: https://github.com/FasterXML/jackson-module-kotlin/issues/131
+class MemoryAllocationCommand(val amount: Int) : Command() {
     companion object {
         const val ESTIMATED_OBJECT_SIZE = 20.5
         const val KB_IN_BYTES = 1000
     }
 
-    private val totalAllocations = ((amount * KB_IN_BYTES) / ESTIMATED_OBJECT_SIZE).roundToInt()
+    private val totalAllocations: Int
     val storage = ArrayList<Any>()
+
+    init {
+        totalAllocations = ((amount * KB_IN_BYTES) / ESTIMATED_OBJECT_SIZE).roundToInt()
+    }
 
 
     override fun run() {
